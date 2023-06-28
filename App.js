8f27ef7880462c6490f292f78c9224aa01d7bb54
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-29 02:28:04 +0530

    basic structure

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+          {/* Header */}
+          <View style={styles.content}>
+            {/* to Form */}
+              <View style={styles.Header}>
+              </View>
+          </View>
     </View>
   );
 }
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
