1a4b5d222e8917af8a73bd4e772727115774942f
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-29 02:47:47 +0530

    basics

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Header from './Component/Header';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'play on the switch', key: '3'},
+  ]);
+
   return (
     <View style={styles.container}>
+          
           {/* Header */}
+          < Header />
+          
           <View style={styles.content}>
-            {/* to Form */}
-              <View style={styles.Header}>
-              </View>
+                {/* to Form */}
+                <View style={styles.list}>
+               
+                 {/* Flat List*/}
+                    <FlatList
+                      data={todos}
+                      renderItem={({item})=>(
+                        <Text>{item.text}</Text>
+                      )}
+                    />
+
+                </View>
+          
           </View>
+
     </View>
   );
 }
@@ -19,4 +41,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  content:{
+    padding: 40,
+  },
+  list:{
+    marginTop: 20,
+  }
 });
